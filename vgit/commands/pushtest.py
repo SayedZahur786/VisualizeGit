b96488346e9ecbb558a57c4cb75e32ea64e840ec
SayedZahur786 <syedzahurulhasan04@gmail.com>
2025-09-15 17:27:17 +0530

    Revert "Added Vgit Animation for git push command"
    This reverts commit 7d01ed0a106ea963f6a58dc18cb46a980e0a0b70.
    
    7d01ed0t

--- vgit/commands/pushtest.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
-from vgit.animations.push import PushAnimation
-
-if __name__ == "__main__":
-    anim = PushAnimation(commits=3, remote='origin', branch='main')
-    anim.animate()
